789f2b3faa09121a397d6fb7bbbc8365c8ee491a
David Barroso <dbarrosop@dravetech.com>
2017-11-22 17:32:46 +0100

    raise BrigadeExecutionException if a task in the pool fails

--- brigade/core/exceptions.py
+++ brigade/core/exceptions.py
@@ -10,8 +10,23 @@ class CommandError(Exception):
     """
 
     def __init__(self, message, command, status_code, stdout, stderr):
-        super().__init__(message)
         self.status_code = status_code
         self.stdout = stdout
         self.stderr = stderr
         self.command = command
+
+
+class BrigadeExecutionException(Exception):
+    """
+    Raised by brigade when any of the tasks managed by :meth:`brigade.core.Brigade.run`
+    when any of the tasks fail.
+
+    Attributes:
+        result (dict): A dictionary with the results for each individual execution.
+          The hosts that raised an exception will contain the resulting exection.
+        failed (list): List of hosts that failed.
+    """
+
+    def __init__(self, result):
+        self.result = result
+        self.failed = [h for h, r in result.items() if isinstance(r, Exception)]
