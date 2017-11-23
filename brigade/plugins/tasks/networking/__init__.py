0d2030dd2a0dce914d0defa593369a697d3f78f7
David Barroso <dbarrosop@dravetech.com>
2017-11-23 16:56:27 +0100

    added napalm_cli task

--- brigade/plugins/tasks/networking/__init__.py
+++ brigade/plugins/tasks/networking/__init__.py
@@ -1,7 +1,9 @@
+from .napalm_cli import napalm_cli
 from .napalm_configure import napalm_configure
 from .napalm_get_facts import napalm_get_facts
 
 __all__ = (
+    "napalm_cli",
     "napalm_configure",
     "napalm_get_facts",
 )
