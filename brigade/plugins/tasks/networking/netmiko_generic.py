2b8ec99b4bf25f17caf0130dc2f2048eaea03514
Kirk Byers <ktbyers@twb-tech.com>
2017-11-24 15:33:19 -0800

    POC netmiko_generic

---
+++ brigade/plugins/tasks/networking/netmiko_generic.py
@@ -0,0 +1,62 @@
+from brigade.core.task import Result
+
+from netmiko import ConnectHandler
+
+napalm_to_netmiko_map = {
+    'ios': 'cisco_ios',
+    'nxos': 'cisco_nxos',
+    'eos': 'arista_eos',
+    'junos': 'juniper_junos',
+    'iosxr': 'cisco_iosxr'
+}
+
+
+def netmiko_generic(task, method, ip=None, host=None, username=None, password=None,
+                    device_type=None, netmiko_dict=None, cmd_args=None, cmd_kwargs=None):
+    """
+    Execute a generic netmiko method.
+
+    Arguments:
+        method(str): method to use
+        hostname (string, optional): defaults to ``brigade_ip``
+        username (string, optional): defaults to ``brigade_username``
+        password (string, optional): defaults to ``brigade_password``
+        driver (string, optional): defaults to ``nos``
+        timeout (int, optional): defaults to 60
+        optional_args (dict, optional): defaults to ``{"port": task.host["napalm_port"]}``
+
+
+    Returns:
+        :obj:`brigade.core.task.Result`:
+          * result (``dict``): dictionary with the result of the getter
+    """
+    parameters = {
+        "username": username or task.host["brigade_username"],
+        "password": password or task.host["brigade_password"],
+    }
+    if host is None and ip is None:
+        parameters["ip"] = task.host["brigade_ip"]
+    elif ip is not None:
+        parameters["ip"] = ip
+    elif host is not None:
+        parameters["host"] = host
+
+    if netmiko_dict is not None:
+        parameters.update(netmiko_dict)
+
+    if device_type is None:
+        device_type = task.host["nos"]
+    # Convert to netmiko format (if napalm format is used)
+    parameters['device_type'] = napalm_to_netmiko_map.get(device_type, device_type)
+
+    net_connect = ConnectHandler(**parameters)
+#    import pdb; pdb.set_trace()
+    if True:
+#    with ConnectHandler(**parameters) as net_connect:
+        netmiko_method = getattr(net_connect, method)
+        if cmd_args is None:
+            cmd_args = ()
+        if cmd_kwargs is None:
+            cmd_kwargs = {}
+        result = netmiko_method(*cmd_args, **cmd_kwargs)
+    return Result(host=task.host, result=result)
