8f79e11b31e8de966db0484d7387ba4d34ec4b56
Patrick Ogenstad <patrick@ogenstad.com>
2017-11-29 13:17:35 +0100

    Add tcp_ping plugin with tests.

--- brigade/plugins/tasks/networking/__init__.py
+++ brigade/plugins/tasks/networking/__init__.py
@@ -2,10 +2,12 @@ from .napalm_cli import napalm_cli
 from .napalm_configure import napalm_configure
 from .napalm_get_facts import napalm_get_facts
 from .netmiko_run import netmiko_run
+from .tcp_ping import tcp_ping
 
 __all__ = (
     "napalm_cli",
     "napalm_configure",
     "napalm_get_facts",
     "netmiko_run",
+    "tcp_ping",
 )
---
+++ tests/tasks/networking/test_tcp_ping.py
@@ -0,0 +1,63 @@
+import os
+
+
+from brigade.core import Brigade
+from brigade.core.exceptions import BrigadeExecutionError
+from brigade.plugins.inventory.simple import SimpleInventory
+from brigade.plugins.tasks import networking
+
+
+import pytest
+
+cur_dir = os.path.dirname(os.path.realpath(__file__))
+ext_inv_file = '{}/../../inventory_data/external_hosts.yaml'.format(cur_dir)
+
+
+class Test(object):
+
+    def test_tcp_ping_port(self, brigade):
+        filter = brigade.filter(name="host4.group_2")
+        result = filter.run(networking.tcp_ping, ports=65004)
+
+        assert result
+        for h, r in result.items():
+            assert r.tcp_port[65004]
+
+    def test_tcp_ping_ports(self, brigade):
+        filter = brigade.filter(name="host4.group_2")
+        result = filter.run(networking.tcp_ping, ports=[35004, 65004])
+
+        assert result
+        for h, r in result.items():
+            assert r.tcp_port[35004] is False
+            assert r.tcp_port[65004]
+
+    def test_tcp_ping_invalid_port(self, brigade):
+        with pytest.raises(BrigadeExecutionError) as e:
+            brigade.run(networking.tcp_ping,
+                        ports='web')
+        assert len(e.value.failed_hosts) == len(brigade.inventory.hosts)
+        for exc in e.value.failed_hosts.values():
+            assert isinstance(exc, ValueError)
+
+    def test_tcp_ping_invalid_ports(self, brigade):
+        with pytest.raises(BrigadeExecutionError) as e:
+            brigade.run(networking.tcp_ping,
+                        ports=[22, 'web', 443])
+        assert len(e.value.failed_hosts) == len(brigade.inventory.hosts)
+        for exc in e.value.failed_hosts.values():
+            assert isinstance(exc, ValueError)
+
+
+def test_tcp_ping_external_hosts():
+    external = Brigade(inventory=SimpleInventory(ext_inv_file), dry_run=True)
+    result = external.run(networking.tcp_ping, ports=[23, 443])
+
+    assert result
+    for h, r in result.items():
+        if h == 'www.github.com':
+            assert r.tcp_port[23] is False
+            assert r.tcp_port[443]
+        else:
+            assert r.tcp_port[23] is False
+            assert r.tcp_port[443] is False
