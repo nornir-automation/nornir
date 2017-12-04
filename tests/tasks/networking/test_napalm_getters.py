76e11d4d37a634a30766b18dcb62945654369fcd
David Barroso <dbarrosop@dravetech.com>
2017-12-04 11:11:58 +0000

    rename napalm_get_facts to napalm_getters

---
+++ tests/tasks/networking/test_napalm_getters.py
@@ -0,0 +1,35 @@
+import os
+
+from brigade.core.exceptions import BrigadeExecutionError
+from brigade.plugins.tasks import networking
+
+import pytest
+
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__)) + "/mocked/napalm_getters"
+
+
+class Test(object):
+
+    def test_napalm_getters(self, brigade):
+        opt = {"path": THIS_DIR + "/test_napalm_getters"}
+        result = brigade.filter(name="dev3.group_2").run(networking.napalm_getters,
+                                                         getters=["facts",
+                                                                  "interfaces"],
+                                                         optional_args=opt)
+        assert result
+        for h, r in result.items():
+            assert r.result["facts"]
+            assert r.result["interfaces"]
+
+    def test_napalm_getters_error(self, brigade):
+        opt = {"path": THIS_DIR + "/test_napalm_getters_error"}
+
+        with pytest.raises(BrigadeExecutionError) as e:
+            brigade.filter(name="dev3.group_2").run(networking.napalm_getters,
+                                                    getters=["facts",
+                                                             "interfaces"],
+                                                    optional_args=opt)
+        assert len(e.value.failed_hosts)
+        for exc in e.value.failed_hosts.values():
+            assert isinstance(exc, KeyError)
