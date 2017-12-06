0625341399c09ba18e9d524f91a02af5d84479c1
David Barroso <dbarrosop@dravetech.com>
2017-12-06 00:42:43 +0000

    migrate scp to sftp/paramiko

--- brigade/plugins/tasks/files/scp.py
+++ brigade/plugins/tasks/files/scp.py
@@ -1,17 +1,33 @@
+import hashlib
 import logging
-import shlex
-import subprocess
 
-
-from brigade.core.exceptions import CommandError
-from brigade.core.helpers import format_string
 from brigade.core.task import Result
+from brigade.plugins.tasks import commands
+
+import paramiko
 
 
 logger = logging.getLogger("brigade")
 
 
-def scp(task, src, dst, ignore_keys=True):
+def get_local_hash(filename):
+    sha1sum = hashlib.sha1()
+
+    with open(filename, 'rb') as f:
+        block = f.read(2**16)
+        while len(block) != 0:
+            sha1sum.update(block)
+            block = f.read(2**16)
+    return sha1sum.hexdigest()
+
+
+def get_remote_hash(task, filename):
+    command = "sha1sum {}".format(filename)
+    result = commands.remote_command(task, command)
+    return result.stdout.split()[0]
+
+
+def scp(task, src, dst):
     """
     Runs scp on the local machine. You can use variables in the path.
 
@@ -34,29 +50,24 @@ def scp(task, src, dst, ignore_keys=True):
     Raises:
         :obj:`brigade.core.exceptions.CommandError`: when there is a command error
     """
-    src = format_string(src, task, **task.host)
-    dst = format_string(dst, task, **task.host)
+    client = task.host.ssh_connection()
+    sftp = paramiko.SFTPClient.from_transport(client.get_transport())
 
-    options = " -o 'User={}' ".format(task.host.username)
+    try:
+        f = sftp.file(dst)
+        found = True
+    except IOError:
+        found = False
 
-    if ignore_keys:
-        options += " -o 'StrictHostKeyChecking=no' "
+    if found:
+        remote_hash = get_remote_hash(task, dst)
+        f.close()
+        local_hash = get_local_hash(src)
+        same = remote_hash == local_hash
+    else:
+        same = False
 
-    command = "sshpass -p '{h.password}' scp -r {options} -P {h.ssh_port} \
-            {src} {dst}".format(h=task.host, options=options, src=src, dst=dst)
+    if not same and not task.dry_run:
+        sftp.put(src, dst)
 
-    logger.debug("{}:cmd:{}".format(task.host, command))
-    ssh = subprocess.Popen(shlex.split(command),
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE,
-                           shell=False)
-
-    stdout, stderr = ssh.communicate()
-    stdout = stdout.decode()
-    stderr = stderr.decode()
-
-    if ssh.poll():
-        raise CommandError(command, ssh.returncode, stdout, stderr)
-
-    result = stderr if stderr else stdout
-    return Result(result=result, host=task.host, stderr=stderr, stdout=stdout)
+    return Result(host=task.host, changed=not same)
--- tests/tasks/files/test_scp.py
+++ tests/tasks/files/test_scp.py
@@ -1,29 +1,35 @@
-from brigade.core.exceptions import BrigadeExecutionError, CommandError
-from brigade.plugins.tasks import commands
+#  from brigade.core.exceptions import BrigadeExecutionError, CommandError
 from brigade.plugins.tasks import files
 
-import pytest
+#  import pytest
 
 
 class Test(object):
 
     def test_scp(self, brigade):
-        brigade.run(files.scp,
-                    src="README.md",
-                    dst="{host.host}:/tmp")
+        brigade.dry_run = True
+        result = brigade.run(files.scp,
+                             src="README.md",
+                             dst="/tmp/README.md")
 
-        # let's check the file is there
-        result = brigade.run(commands.remote_command,
-                             command="ls /tmp/README.md")
         assert result
         for h, r in result.items():
-            assert r.stdout == "/tmp/README.md\n"
+            assert r.changed
 
-    def test_scp_error_generic(self, brigade):
-        with pytest.raises(BrigadeExecutionError) as e:
-            brigade.run(files.scp,
-                        src="README.md",
-                        dst="{host.host}:/asdasd/tmp")
-        assert len(e.value.failed_hosts) == len(brigade.inventory.hosts)
-        for exc in e.value.failed_hosts.values():
-            assert isinstance(exc, CommandError)
+        brigade.dry_run = False
+        result = brigade.run(files.scp,
+                             src="README.md",
+                             dst="/tmp/README.md")
+
+        assert result
+        for h, r in result.items():
+            assert r.changed
+
+        brigade.dry_run = True
+        result = brigade.run(files.scp,
+                             src="README.md",
+                             dst="/tmp/README.md")
+
+        assert result
+        for h, r in result.items():
+            assert not r.changed
