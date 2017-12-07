4dad7b8457dbf1044990ce14ec8f2f8b4caee6f6
Patrick Ogenstad <patrick@ogenstad.com>
2017-12-07 17:23:52 +0100

    Move num_workers and raise_on_error to config

--- brigade/core/configuration.py
+++ brigade/core/configuration.py
@@ -1,16 +1,28 @@
+import ast
 import os
+
+
 import yaml
 
 
 CONF = {
     'num_workers': {
         'description': 'Number of Brigade worker processes',
-        'environment': 'BRIGADE_NUM_WORKERS',
+        'env': 'BRIGADE_NUM_WORKERS',
+        'type': int,
         'default': 20,
+    },
+    'raise_on_error': {
+        'description': "If set to ``True``, :meth:`run` method of will raise an exception if at "
+                       "least a host failed.",
+        'env': 'BRIGADE_RAISE_ON_ERROR',
+        'type': bool,
+        'default': True,
     },
     'ssh_config_file': {
         'description': 'User ssh_config_file',
-        'environment': 'BRIGADE_SSH_CONFIG_FILE',
+        'env': 'BRIGADE_SSH_CONFIG_FILE',
+        'type': str,
         'default': os.path.join(os.path.expanduser("~"), ".ssh", "config"),
     },
 }
@@ -35,5 +47,15 @@ class Config:
     def _assign_properties(self, c):
 
         for p in CONF:
-            v = os.environ.get(CONF[p]['environment']) or c.get(p) or CONF[p]['default']
+            if CONF[p]['type'] == bool:
+                if os.environ.get(CONF[p]['env']) is not None:
+                    v = os.environ.get(CONF[p]['env'])
+                elif c.get(p) is not None:
+                    v = c.get(p)
+                else:
+                    v = CONF[p]['default']
+                v = ast.literal_eval(str(v).title())
+            else:
+                v = os.environ.get(CONF[p]['env']) or c.get(p) or CONF[p]['default']
+                v = CONF[p]['type'](v)
             setattr(self, p, v)
