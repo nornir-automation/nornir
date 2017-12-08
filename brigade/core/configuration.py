1dfc84e059181a265205c11d4a911aaf429be851
Patrick Ogenstad <patrick@ogenstad.com>
2017-12-08 12:17:39 +0100

    Add BRIGADE_ as default prefix to variables

--- brigade/core/configuration.py
+++ brigade/core/configuration.py
@@ -8,20 +8,17 @@ import yaml
 CONF = {
     'num_workers': {
         'description': 'Number of Brigade worker processes',
-        'env': 'BRIGADE_NUM_WORKERS',
         'type': 'int',
         'default': 20,
     },
     'raise_on_error': {
         'description': "If set to ``True``, (:obj:`brigade.core.Brigade.run`) method of will raise "
                        "an exception if at least a host failed.",
-        'env': 'BRIGADE_RAISE_ON_ERROR',
         'type': 'bool',
         'default': True,
     },
     'ssh_config_file': {
         'description': 'User ssh_config_file',
-        'env': 'BRIGADE_SSH_CONFIG_FILE',
         'type': 'str',
         'default': os.path.join(os.path.expanduser("~"), ".ssh", "config"),
         'default_doc': '~/.ssh/config'
@@ -53,15 +50,17 @@ class Config:
     def _assign_properties(self, c):
 
         for p in CONF:
+            env = CONF[p].get('env') or 'BRIGADE_' + p.upper()
+            print(env)
             if CONF[p]['type'] == 'bool':
-                if os.environ.get(CONF[p]['env']) is not None:
-                    v = os.environ.get(CONF[p]['env'])
+                if os.environ.get(env) is not None:
+                    v = os.environ.get(env)
                 elif c.get(p) is not None:
                     v = c.get(p)
                 else:
                     v = CONF[p]['default']
                 v = ast.literal_eval(str(v).title())
             else:
-                v = os.environ.get(CONF[p]['env']) or c.get(p) or CONF[p]['default']
+                v = os.environ.get(env) or c.get(p) or CONF[p]['default']
                 v = types[CONF[p]['type']](v)
             setattr(self, p, v)
