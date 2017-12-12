36f65214432fa91ebb5afff28c6070d20ec239cc
Kirk Byers <ktbyers@twb-tech.com>
2017-12-11 22:15:54 -0800

    Fixing netmiko test arguments to be proper form

--- tests/tasks/networking/test_netmiko_run.py
+++ tests/tasks/networking/test_netmiko_run.py
@@ -4,9 +4,10 @@ from brigade.plugins.tasks import networking
 class Test(object):
 
     def test_netmiko_run(self, brigade):
+        cmd_kwargs = {'command_string': 'hostname'}
         result = brigade.filter(name="dev4.group_2").run(networking.netmiko_run,
                                                          method="send_command",
-                                                         command_string="hostname")
+                                                         cmd_kwargs=cmd_kwargs)
         assert result
         for h, r in result.items():
             assert h == r.result.strip()
