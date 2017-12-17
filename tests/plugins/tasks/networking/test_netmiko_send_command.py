a0d27b7708fde90dc3e8515d298616e43db3d049
Kirk Byers <ktbyers@twb-tech.com>
2017-12-16 18:26:40 -0800

    Add connection setup

--- tests/plugins/tasks/networking/test_netmiko_send_command.py
+++ tests/plugins/tasks/networking/test_netmiko_send_command.py
@@ -4,6 +4,7 @@ from brigade.plugins.tasks import networking
 class Test(object):
 
     def test_netmiko_send_command(self, brigade):
+        brigade.filter(name="dev4.group_2").run(task=netmiko_connection)
         result = brigade.filter(name="dev4.group_2").run(networking.netmiko_send_command,
                                                          command_string="hostname")
         assert result
