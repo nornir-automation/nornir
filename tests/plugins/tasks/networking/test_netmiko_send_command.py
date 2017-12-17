71e6666cfbd5ba3bf376e656752d67fea440d895
Kirk Byers <ktbyers@twb-tech.com>
2017-12-16 18:06:17 -0800

    Fixing unit test

--- tests/plugins/tasks/networking/test_netmiko_send_command.py
+++ tests/plugins/tasks/networking/test_netmiko_send_command.py
@@ -4,13 +4,13 @@ from brigade.plugins.tasks import networking
 class Test(object):
 
     def test_netmiko_send_command(self, brigade):
-        result = brigade.filter(name="dev4.group_2").run(networking.netmiko_send_command
+        result = brigade.filter(name="dev4.group_2").run(networking.netmiko_send_command,
                                                          command_string="hostname")
         assert result
         for h, r in result.items():
             assert h == r.result.strip()
 
-        result = brigade.filter(name="dev4.group_2").run(networking.netmiko_send_command
+        result = brigade.filter(name="dev4.group_2").run(networking.netmiko_send_command,
                                                          command_string="hostname",
                                                          use_timing=True)
         assert result
