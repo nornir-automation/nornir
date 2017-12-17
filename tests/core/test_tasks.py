444eaaf845cddd4cd9fced33f5dacdf908f7cc0e
David Barroso <dbarrosop@dravetech.com>
2017-12-17 20:57:37 +0100

    added test for hosts that fail and have to be skipped

--- tests/core/test_tasks.py
+++ tests/core/test_tasks.py
@@ -1,6 +1,16 @@
 from brigade.plugins.tasks import commands
 
 
+def task_fails_for_some(task):
+    if task.host.name == "dev3.group_2":
+        # let's hardcode a failure
+        return task.run(commands.command,
+                        command="sasdasdasd")
+    else:
+        return task.run(commands.command,
+                        command="echo {}".format(task.host))
+
+
 def sub_task(task):
     return task.run(commands.command,
                     command="echo {}".format(task.host))
@@ -20,3 +30,23 @@ class Test(object):
         assert result
         for h, r in result.items():
             assert h == r.stdout.strip()
+
+    def test_skip_failed_host(self, brigade):
+        brigade.config.raise_on_error = False
+        result = brigade.run(task_fails_for_some)
+        for h, r in result.items():
+            assert h == r.stdout.strip()
+
+        # We can set it to True already because the task
+        # shouldn't be run on the host that fails
+        brigade.config.raise_on_error = True
+
+        result = brigade.run(task_fails_for_some)
+        for h, r in result.items():
+            if h == "dev3.group_2":
+                assert r.skipped
+            else:
+                assert h == r.stdout.strip()
+
+        # let's reset it
+        brigade.data.failed_hosts = set()
