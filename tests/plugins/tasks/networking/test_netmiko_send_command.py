0bae6c5e2850b01d73bf5a04e4f5ab288973bc59
Kirk Byers <ktbyers@twb-tech.com>
2017-12-19 14:45:53 -0800

    Update unit tests for netmiko

--- tests/plugins/tasks/networking/test_netmiko_send_command.py
+++ tests/plugins/tasks/networking/test_netmiko_send_command.py
@@ -3,8 +3,15 @@ from brigade.plugins.tasks import connections, networking
 
 class Test(object):
 
-    def test_netmiko_send_command(self, brigade):
+    def test_explicit_netmiko_connection(self, brigade):
         brigade.filter(name="dev4.group_2").run(task=connections.netmiko_connection)
+        result = brigade.filter(name="dev4.group_2").run(networking.netmiko_send_command,
+                                                         command_string="hostname")
+        assert result
+        for h, r in result.items():
+            assert h == r.result.strip()
+
+    def test_netmiko_send_command(self, brigade):
         result = brigade.filter(name="dev4.group_2").run(networking.netmiko_send_command,
                                                          command_string="hostname")
         assert result
