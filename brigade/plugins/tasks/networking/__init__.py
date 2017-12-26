3c6d23f4a44c96aa8db232b9b1b830e816676bf0
Kirk Byers <ktbyers@twb-tech.com>
2017-12-26 02:12:06 -0800

    Add Netmiko send_config task

--- brigade/plugins/tasks/networking/__init__.py
+++ brigade/plugins/tasks/networking/__init__.py
@@ -2,6 +2,7 @@ from .napalm_cli import napalm_cli
 from .napalm_configure import napalm_configure
 from .napalm_get import napalm_get
 from .netmiko_send_command import netmiko_send_command
+from .netmiko_send_config import netmiko_send_config
 from .tcp_ping import tcp_ping
 
 __all__ = (
@@ -9,5 +10,6 @@ __all__ = (
     "napalm_configure",
     "napalm_get",
     "netmiko_send_command",
+    "netmiko_send_config",
     "tcp_ping",
 )
