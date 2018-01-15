b9a0d09db4ed7dc9e422f99ae28e6d3cf5a46457
David Barroso <dbarrosop@dravetech.com>
2018-01-15 22:01:49 +0100

    allow printing multiresult objects

--- brigade/plugins/tasks/text/print_result.py
+++ brigade/plugins/tasks/text/print_result.py
@@ -1,20 +1,23 @@
 import pprint
 
-from brigade.core.task import AggregatedResult, Result
+from brigade.core.task import AggregatedResult, MultiResult, Result
 
 from colorama import Fore, Style, init
 
 
-init(autoreset=True)
+init(autoreset=True, convert=False, strip=False)
 
 
-def print_result(task, data, vars=None):
+def print_result(task, data, vars=None, failed=None, task_id=None):
     """
     Prints on screen the :obj:`brigade.core.task.Result` from a previous task
 
     Arguments:
         data (:obj:`brigade.core.task.Result`): from a previous task
         vars (list of str): Which attributes you want to print
+        failed (``bool``): if ``True`` assume the task failed
+        task_id (``int``): if we have a :obj:`brigade.core.task.MultiResult` print
+            only task in this position
 
     Returns:
         :obj:`brigade.core.task.Result`:
@@ -26,7 +29,12 @@ def print_result(task, data, vars=None):
     if isinstance(data, AggregatedResult):
         data = data[task.host.name]
 
-    if data.failed:
+    if task_id is not None:
+        r = data[task_id]
+        data = MultiResult(data.name)
+        data.append(r)
+
+    if data.failed or failed:
         color = Fore.RED
     elif data.changed:
         color = Fore.YELLOW
@@ -41,9 +49,10 @@ def print_result(task, data, vars=None):
         print("{}{}{}{}".format(Style.BRIGHT, Fore.CYAN, msg, "-" * (80 - len(msg))))
         for v in vars:
             x = getattr(r, v, "")
-            if r and not isinstance(x, str):
-                pprint.pprint(x)
-            elif r:
+            if x and not isinstance(x, str):
+                pprint.pprint(x, indent=2)
+            elif x:
                 print(x)
+    print()
 
     return Result(task.host)
