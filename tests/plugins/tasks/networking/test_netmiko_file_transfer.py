a72c6e5bc6af749ae9c1fd02150cf108706b6601
Kirk Byers <ktbyers@twb-tech.com>
2018-03-09 09:19:45 -0800

    Fixing spelling error

--- tests/plugins/tasks/networking/test_netmiko_file_transfer.py
+++ tests/plugins/tasks/networking/test_netmiko_file_transfer.py
@@ -5,7 +5,7 @@ class Test(object):
     def test_netmiko_file_transfer(self, brigade):
         source_file = 'test_file.txt'
         dest_file = 'test_file.txt'
-        result = brigade.filter(name="dev4.group_2").run(networking.netmiko_fle_transfer,
+        result = brigade.filter(name="dev4.group_2").run(networking.netmiko_file_transfer,
                                                          source_file=source_file,
                                                          dest_file=dest_file,
                                                          direction='put')
