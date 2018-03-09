07f0f6ae45fb9054d8df2794a7a06d898ed7c81b
Kirk Byers <ktbyers@twb-tech.com>
2018-03-08 19:46:55 -0800

    Netmiko secure copy file_transfer task

--- brigade/plugins/tasks/networking/__init__.py
+++ brigade/plugins/tasks/networking/__init__.py
@@ -2,6 +2,7 @@ from .napalm_cli import napalm_cli
 from .napalm_configure import napalm_configure
 from .napalm_get import napalm_get
 from .napalm_validate import napalm_validate
+from .netmiko_file_transfer import netmiko_file_transfer
 from .netmiko_send_command import netmiko_send_command
 from .netmiko_send_config import netmiko_send_config
 from .tcp_ping import tcp_ping
@@ -11,6 +12,7 @@ __all__ = (
     "napalm_configure",
     "napalm_get",
     "napalm_validate",
+    "netmiko_file_transfer",
     "netmiko_send_command",
     "netmiko_send_config",
     "tcp_ping",
