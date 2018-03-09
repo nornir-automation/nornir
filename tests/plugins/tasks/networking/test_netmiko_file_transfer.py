fd3672835262a2a04ef8ca84fcbbbc2a2883d725
Kirk Byers <ktbyers@twb-tech.com>
2018-03-08 19:56:34 -0800

    Unit tests for Netmiko file transfer

---
+++ tests/plugins/tasks/networking/test_netmiko_file_transfer.py
@@ -0,0 +1,15 @@
+from brigade.plugins.tasks import networking
+
+
+class Test(object):
+    def test_netmiko_send_command(self, brigade):
+        source_file = 'test_file.txt'
+        dest_file = 'test_file.txt'
+        result = brigade.filter(name="dev4.group_2").run(networking.netmiko_fle_transfer,
+                                                         source_file=source_file,
+                                                         dest_file=dest_file,
+                                                         direction='put')
+        assert result
+        for h, r in result.items():
+            assert r.result
+            assert r.changed
