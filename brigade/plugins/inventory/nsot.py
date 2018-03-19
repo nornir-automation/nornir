33915e0fc24a9de5b850d006feebecd1b498aae5
smnmtzgr <smnmtzgr@gmail.com>
2018-03-19 11:43:08 +0100

    support more nsot parameters and auth_methods

--- brigade/plugins/inventory/nsot.py
+++ brigade/plugins/inventory/nsot.py
@@ -15,22 +15,38 @@ class NSOTInventory(Inventory):
         the name of the site the host belongs to.
 
     Environment Variables:
-        * ``NSOT_URL``: URL to nsot's API (defaults to ``http://localhost:8990/api``)
-        * ``NSOT_EMAIL``: email for authentication (defaults to admin@acme.com)
+        * ``nsot_url``: URL to nsot's API (defaults to ``http://localhost:8990/api``)
+        * ``nsot_email``: email for authentication (defaults to admin@acme.com)
+        * ``nsot_auth_method``: auth_header or auth_token
+        * ``nsot_auth_header``: used for auth_header authentication method
+        * ``nsot_secret_key``: needed for auth_method = auth_token
 
     Arguments:
         flatten_attributes (bool): Assign host attributes to the root object. Useful
             for filtering hosts.
     """
 
-    def __init__(self, flatten_attributes=True, **kwargs):
-        NSOT_URL = os.environ.get('NSOT_URL', 'http://localhost:8990/api')
-        NSOT_EMAIL = os.environ.get('NSOT_EMAIL', 'admin@acme.com')
+    def __init__(self, nsot_url="", nsot_email="", nsot_auth_method="",
+                 nsot_secret_key="", nsot_auth_header="", flatten_attributes=True, **kwargs):
+        nsot_url = nsot_url or os.environ.get('NSOT_URL', 'http://localhost:8990/api')
+        nsot_email = nsot_email or os.environ.get('NSOT_EMAIL', 'admin@acme.com')
+        nsot_auth_method = nsot_auth_method or os.environ.get('NSOT_AUTH_METHOD', 'auth_header')
 
-        headers = {'X-NSoT-Email': NSOT_EMAIL}
-        devices = requests.get('{}/devices'.format(NSOT_URL), headers=headers).json()
-        sites = requests.get('{}/sites'.format(NSOT_URL), headers=headers).json()
-        interfaces = requests.get('{}/interfaces'.format(NSOT_URL), headers=headers).json()
+        if nsot_auth_method == "auth_token":
+            nsot_secret_key = nsot_secret_key or os.environ.get('NSOT_SECRET_KEY', 'empty')
+            data = {'email': nsot_email, 'secret_key': nsot_secret_key}
+            res = requests.post('{}/authenticate/'.format(nsot_url), data=data)
+            auth_token = res.json().get('auth_token')
+            headers = {'Authorization': 'AuthToken {}:{}'.format(nsot_email, auth_token)}
+
+        else:
+            nsot_auth_header = nsot_auth_header or os.environ.get('NSOT_AUTH_HEADER',
+                                                                  'X-NSoT-Email')
+            headers = {nsot_auth_header: nsot_email}
+
+        devices = requests.get('{}/devices'.format(nsot_url), headers=headers).json()
+        sites = requests.get('{}/sites'.format(nsot_url), headers=headers).json()
+        interfaces = requests.get('{}/interfaces'.format(nsot_url), headers=headers).json()
 
         # We resolve site_id and assign "site" variable with the name of the site
         for d in devices:
