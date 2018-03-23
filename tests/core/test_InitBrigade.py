43097ef0be7efefc76d818fcaadedd9daa7ff8d9
smnmtzgr <smnmtzgr@gmail.com>
2018-03-23 13:25:28 +0100

    implemented tests for inventory and transform_function

--- tests/core/test_InitBrigade.py
+++ tests/core/test_InitBrigade.py
@@ -1,7 +1,7 @@
 import os
 
 from brigade.core import InitBrigade
-from brigade.core.plugins.inventory.nsot import NSOTInventory
+from brigade.plugins.inventory.nsot import NSOTInventory
 
 
 dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_InitBrigade")
