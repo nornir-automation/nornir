382029fc114bf00ef092e99ec6a3e7a8e3706f40
David Barroso <dbarrosop@dravetech.com>
2018-03-31 18:25:28 +0200

    add support for nested subgroups and severity_levels

--- brigade/plugins/functions/text/__init__.py
+++ brigade/plugins/functions/text/__init__.py
@@ -17,6 +17,33 @@ def print_title(title):
     print("{}{}{}{}".format(Style.BRIGHT, Fore.GREEN, msg, "*" * (80 - len(msg))))
 
 
+def _get_color(result, failed):
+    if result.failed or failed:
+        color = Fore.RED
+    elif result.changed:
+        color = Fore.YELLOW
+    else:
+        color = Fore.GREEN
+    return color
+
+
+def _print_individual_result(result, host, vars, failed, severity_level, task_group=False):
+    if result.severity < severity_level:
+        return
+    color = _get_color(result, failed)
+    subtitle = "" if result.changed is None else " ** changed : {} ".format(result.changed)
+    level_name = logging.getLevelName(result.severity)
+    symbol = "v" if task_group else "-"
+    msg = "{} {}{}".format(symbol * 4, result.name, subtitle)
+    print("{}{}{}{} {}".format(Style.BRIGHT, color, msg, symbol * (80 - len(msg)), level_name))
+    for v in vars:
+        x = getattr(result, v, "")
+        if x and not isinstance(x, str):
+            pprint.pprint(x, indent=2)
+        elif x:
+            print(x)
+
+
 def print_result(result, host=None, vars=None, failed=None, severity_level=logging.INFO):
     """
     Prints on screen the :obj:`brigade.core.task.Result` from a previous task
@@ -50,25 +77,11 @@ def print_result(result, host=None, vars=None, failed=None, severity_level=loggi
             print("{}{}{}{}".format(Style.BRIGHT, Fore.BLUE, msg, "*" * (80 - len(msg))))
             print_result(host_data, host, vars, failed, severity_level)
     elif isinstance(result, MultiResult):
-        for r in result:
+        _print_individual_result(result[0], host, vars, failed, severity_level, task_group=True)
+        for r in result[1:]:
             print_result(r, host, vars, failed, severity_level)
-        print()
+        color = _get_color(result[0], failed)
+        msg = "^^^^ END {} ".format(result[0].name)
+        print("{}{}{}{}".format(Style.BRIGHT, color, msg, "^" * (80 - len(msg))))
     elif isinstance(result, Result):
-        if result.severity < severity_level:
-            return
-        if result.failed or failed:
-            color = Fore.RED
-        elif result.changed:
-            color = Fore.YELLOW
-        else:
-            color = Fore.GREEN
-        subtitle = "" if result.changed is None else " ** changed : {} ".format(result.changed)
-        level_name = logging.getLevelName(result.severity)
-        msg = "---- {}{}".format(result.name, subtitle)
-        print("{}{}{}{}- {}".format(Style.BRIGHT, color, msg, "-" * (80 - len(msg)), level_name))
-        for v in vars:
-            x = getattr(result, v, "")
-            if x and not isinstance(x, str):
-                pprint.pprint(x, indent=2)
-            elif x:
-                print(x)
+        _print_individual_result(result, host, vars, failed, severity_level)
