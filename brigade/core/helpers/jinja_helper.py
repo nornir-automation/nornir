260393deefab85ae0629eeaaf745945ad81a94c4
David Barroso <dbarrosop@dravetech.com>
2018-04-10 12:55:35 +0200

    add mechanism to pass jinja_filters (#113)

--- brigade/core/helpers/jinja_helper.py
+++ brigade/core/helpers/jinja_helper.py
@@ -1,16 +1,19 @@
 from jinja2 import Environment, FileSystemLoader, StrictUndefined
 
 
-def render_from_file(path, template, **kwargs):
+def render_from_file(path, template, jinja_filters=None, **kwargs):
+    jinja_filters = jinja_filters or {}
     env = Environment(
         loader=FileSystemLoader(path), undefined=StrictUndefined, trim_blocks=True
     )
-    #  env.filters.update(jinja_filters.filters())
+    env.filters.update(jinja_filters)
     template = env.get_template(template)
     return template.render(**kwargs)
 
 
-def render_from_string(template, **kwargs):
+def render_from_string(template, jinja_filters=None, **kwargs):
+    jinja_filters = jinja_filters or {}
     env = Environment(undefined=StrictUndefined, trim_blocks=True)
+    env.filters.update(jinja_filters)
     template = env.from_string(template)
     return template.render(**kwargs)
