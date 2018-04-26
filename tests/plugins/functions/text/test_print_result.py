748aa8c3614556cdc15d227a1cac8b207bcdd2df
Patrick Ogenstad <patrick@ogenstad.com>
2018-04-26 22:03:45 +0200

    Add test for print_result function

---
+++ tests/plugins/functions/text/test_print_result.py
@@ -0,0 +1,16 @@
+from brigade.plugins.functions.text import print_result
+from brigade.core.task import Result
+from tests.wrapper import wrap_cli_test
+
+
+def echo_task(task):
+    return Result(host=task.host, result='Hello from Brigade')
+
+
+class Test(object):
+
+    @wrap_cli_test(output="tests/plugins/functions/text/test_data/output")
+    def test_print_output(self, brigade):
+        filter = brigade.filter(name="dev1.group_1")
+        result = filter.run(echo_task)
+        print_result(result)
