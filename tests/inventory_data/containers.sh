41c2766460f3920018ee6b9b05d5ed89955b12a5
David Barroso <dbarrosop@dravetech.com>
2018-04-28 13:42:39 +0200

    run sphinx/black tests as separate jobs (#127)
    * test black and sphinx on different jobs
    
    * use httpbin container to avoid depending on external resources

--- tests/inventory_data/containers.sh
+++ tests/inventory_data/containers.sh
@@ -5,6 +5,8 @@ start () {
     docker run -d -p 65002:22 --name dev2.group_1 --hostname=dev2.group_1 dbarroso/stupid_ssh_container
     docker run -d -p 65003:22 --name dev3.group_2 --hostname=dev3.group_2 dbarroso/stupid_ssh_container
     docker run -d -p 65004:22 --name dev4.group_2 --hostname=dev4.group_2 dbarroso/stupid_ssh_container
+    docker run -d -p 65080:80 --name httpbin bungoume/httpbin-container
+    sleep 3
 }
 
 stop () {
@@ -12,6 +14,7 @@ stop () {
     docker rm -f dev2.group_1
     docker rm -f dev3.group_2
     docker rm -f dev4.group_2
+    docker rm -f httpbin
 }
 
 if [ "$1" == "start" ]; then
