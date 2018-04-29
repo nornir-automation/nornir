bd14734bf1f2ceb62b17ac8651d6ed0116627d94
Patrick Ogenstad <patrick@ogenstad.com>
2018-04-29 10:45:50 +0200

    More tests

--- tests/plugins/functions/text/test_print_result.py
+++ tests/plugins/functions/text/test_print_result.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from brigade.plugins.functions.text import print_result
 from brigade.plugins.functions.text import print_title
@@ -19,6 +20,12 @@ def data_with_greeting(task):
     task.run(task=load_yaml, file="{}/sample.yaml".format(data_dir))
 
 
+def read_data(task):
+    task.run(task=echo_task)
+    r = task.run(task=load_yaml, file="{}/{}.yaml".format(data_dir, task.host))
+    return Result(host=task.host, result=r.result["data"], changed=r.result["changed"])
+
+
 class Test(object):
 
     @wrap_cli_test(output="{}/basic_single".format(output_dir))
@@ -32,8 +39,24 @@ class Test(object):
         result = brigade.run(echo_task)
         print_result(result)
 
+    @wrap_cli_test(output="{}/basic_inventory_one_host".format(output_dir))
+    def test_print_basic_inventory_one_host(self, brigade):
+        result = brigade.run(data_with_greeting)
+        print_result(result["dev2.group_1"])
+
+    @wrap_cli_test(output="{}/basic_inventory_one_task".format(output_dir))
+    def test_print_basic_inventory_one_host(self, brigade):
+        result = brigade.run(data_with_greeting)
+        print_result(result["dev2.group_1"][1])
+
     @wrap_cli_test(output="{}/multiple_tasks".format(output_dir))
     def test_print_multiple_tasks(self, brigade):
         result = brigade.run(data_with_greeting)
         print_title("Behold the data!")
+        print_result(result)
+
+    @wrap_cli_test(output="{}/changed_host".format(output_dir))
+    def test_print_changed_host(self, brigade):
+        filter = brigade.filter(site="site1")
+        result = filter.run(read_data)
         print_result(result)
