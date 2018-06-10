5bd8def21de93191aff162a1b7708a7f77a29774
targuan <targuan@gmail.com>
2018-06-10 13:07:55 +0000

    Black

--- nornir/plugins/inventory/helpers/filters.py
+++ nornir/plugins/inventory/helpers/filters.py
@@ -1,22 +1,22 @@
 class F(object):
-    AND="AND"
-    OR="OR"
-    NOT="NOT"
-    
+    AND = "AND"
+    OR = "OR"
+    NOT = "NOT"
+
     def __init__(self, **kwargs):
         self.operator = self.AND
         self.operands = []
         self.filters = list(kwargs.items())
-    
+
     def match(self, object):
         if self.operator == self.AND:
             reducer_func = all
         elif self.operator == self.OR:
             reducer_func = any
-        matching_result = [o.match(object) for o in self.operands] 
-        matching_result += [object.get(k)==v for k,v in self.filters]
+        matching_result = [o.match(object) for o in self.operands]
+        matching_result += [object.get(k) == v for k, v in self.filters]
         return reducer_func(matching_result)
-    
+
     def __and__(self, other):
         res = F()
         if self.operator == self.AND and other.operator == self.AND:
@@ -27,7 +27,7 @@ class F(object):
             res.operator = self.AND
             res.operands = [self, other]
         return res
-    
+
     def __or__(self, other):
         res = F()
         if self.operator == self.OR and other.operator == self.OR:
