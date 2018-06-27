7acb77819bef27fe03dddc0068541f45cd62dfe3
targuan <targuan@gmail.com>
2018-06-27 22:58:26 +0200

    Change test pattern

--- tests/core/test_filter.py
+++ tests/core/test_filter.py
@@ -1,44 +1,43 @@
+import os
+
 from nornir.core.filter import F
+from nornir.plugins.inventory.simple import SimpleInventory
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+inventory = SimpleInventory(
+    "{}/../inventory_data/hosts.yaml".format(dir_path),
+    "{}/../inventory_data/groups.yaml".format(dir_path),
+)
 
 
+class Test(object):
+    def test_simple(self):
+        f = F(site="site1")
+        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        assert filtered == ["dev1.group_1", "dev2.group_1"]
 
-def test_simple(nornir):
-    f = F(site="site1")
-    filtered = sorted(list((nornir.filter(filter_func=f).inventory.hosts.keys())))
+    def test_and(self):
+        f = F(site="site1") & F(role="www")
+        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        assert filtered == ["dev1.group_1"]
 
-    assert filtered == ["dev1.group_1", "dev2.group_1"]
+    def test_or(self):
+        f = F(site="site1") | F(role="www")
+        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
 
+        assert filtered == ["dev1.group_1", "dev2.group_1", "dev3.group_2"]
 
-def test_and(nornir):
-    f = F(site="site1") & F(role="www")
-    filtered = sorted(list((nornir.filter(filter_func=f).inventory.hosts.keys())))
+    def test_combined(self):
+        f = F(site="site2") | (F(role="www") & F(my_var="comes_from_dev1.group_1"))
+        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        assert filtered == ["dev1.group_1", "dev3.group_2", "dev4.group_2"]
 
-    assert filtered == ["dev1.group_1"]
+    def test_contains(self):
+        f = F(groups__contains="group_1")
+        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        assert filtered == ["dev1.group_1", "dev2.group_1"]
 
-
-def test_or(nornir):
-    f = F(site="site1") | F(role="www")
-    filtered = sorted(list((nornir.filter(filter_func=f).inventory.hosts.keys())))
-
-    assert filtered == ["dev1.group_1", "dev2.group_1", "dev3.group_2"]
-
-
-def test_combined(nornir):
-    f = F(site="site2") | (F(role="www") & F(my_var="comes_from_dev1.group_1"))
-    filtered = sorted(list((nornir.filter(filter_func=f).inventory.hosts.keys())))
-
-    assert filtered == ["dev1.group_1", "dev3.group_2", "dev4.group_2"]
-
-
-def test_contains(nornir):
-    f = F(groups__contains="group_1")
-    filtered = sorted(list((nornir.filter(filter_func=f).inventory.hosts.keys())))
-
-    assert filtered == ["dev1.group_1", "dev2.group_1"]
-
-
-def test_negate(nornir):
-    f = ~F(groups__contains="group_1")
-    filtered = sorted(list((nornir.filter(filter_func=f).inventory.hosts.keys())))
-
-    assert filtered == ["dev3.group_2", "dev4.group_2"]
+    def test_negate(self):
+        f = ~F(groups__contains="group_1")
+        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        assert filtered == ["dev3.group_2", "dev4.group_2"]
