b542e945bf5a33ee49803b053641aba98874af12
Brady Lamprecht <bl839s@att.com>
2018-07-06 21:01:05 -0600

    Potential beta release of using 'NetBox' as a back-end for 'Nornir'
    Squashed commit of the following:
    
    commit b281dc61993fd2385febad9b90f168d04f301341
    Author: Brady Lamprecht <bl839s@att.com>
    Date:   Fri Jul 6 20:57:12 2018 -0600
    
        Added option to flatten custom fields
    
    commit 6c2bbbc4a1808aa1b3a3c9252294828c54d3ba3f
    Author: Brady Lamprecht <bl839s@att.com>
    Date:   Fri Jul 6 19:45:55 2018 -0600
    
        Removed the code for 'device_role` group as you can filter by attributes
    
    commit 65aa4adabb9fa808fa18295a72f6f123bc117d62
    Author: Brady Lamprecht <bl839s@att.com>
    Date:   Fri Jul 6 00:17:49 2018 -0600
    
        Forgot to pass 'netbox.py' through 'Black'
    
    commit 011b6b8ebde27a4a0e467d139b2ca6f08bb35313
    Author: Brady Lamprecht <bl839s@att.com>
    Date:   Fri Jul 6 00:15:53 2018 -0600
    
        Instead of '.serialize()', now only use relevant attributes

--- nornir/plugins/inventory/netbox.py
+++ nornir/plugins/inventory/netbox.py
@@ -4,50 +4,58 @@ from nornir.core.inventory import Inventory
 
 
 class NBInventory(Inventory):
-    def __init__(self, url="", token="", **kwargs):
+    def __init__(
+        self,
+        nb_url=None,
+        nb_token=None,
+        use_slugs=True,
+        flatten_custom_fields=True,
+        **kwargs
+    ):
 
-        url = url or os.environ.get("NB_URL", "http://localhost:8080")
-        token = token or os.environ.get(
+        nb_url = nb_url or os.environ.get("NB_URL", "http://localhost:8080")
+        nb_token = nb_token or os.environ.get(
             "NB_TOKEN", "0123456789abcdef0123456789abcdef01234567"
         )
-        nb = pynetbox.api(url, token)
+        nb_obj = pynetbox.api(nb_url, nb_token)
 
         # Create dict of hosts using 'devices' from NetBox
         devices = {}
-        nb_devices = nb.dcim.devices.all()
-        for item in nb_devices:
+        nb_devices = nb_obj.dcim.devices.all()
+        for d in nb_devices:
 
-            # Serialize item into temporary dict
-            temp = item.serialize()
+            # Add values to temporary dict that don't have a 'slug' attribute
+            temp = {}
+            temp["site"] = d.site.name
+            temp["serial"] = d.serial
+            temp["vendor"] = d.device_type.manufacturer.name
+            temp["asset_tag"] = d.asset_tag
+            temp["nornir_host"] = str(d.primary_ip).split("/")[0]
 
-            # Get value to use for key in outer dict
-            key = temp["name"]
+            if flatten_custom_fields:
+                for cf, value in d.custom_fields.items():
+                    temp[cf] = value
+            else:
+                temp["custom_fields"] = d.custom_fields
 
-            # Delete key 'name' from dict if exists
-            if temp["name"]:
-                del temp["name"]
+            # Add values to temporary dict that do have a 'slug' attribute
+            # This is determined based off of passed variable 'use_slugs'
+            if use_slugs:
+                temp["role"] = d.device_role.slug
+                temp["model"] = d.device_type.slug
 
-            # Populate outer dict value with previously determined key
-            devices[key] = temp
+                # Attempt to add 'platform' based of value in 'slug'
+                try:
+                    temp["nornir_nos"] = d.platform.slug
+                except AttributeError:
+                    temp["nornir_nos"] = None
+            else:
+                temp["role"] = d.device_role
+                temp["model"] = d.device_type
+                temp["nornir_nos"] = d.platform
 
-        # Create group based of 'device_roles' from NetBox
-        roles = {}
-        nb_roles = nb.dcim.device_roles.all()
-        for item in nb_roles:
+            # Copy temporary dict to outer dict
+            devices[d.name] = temp
 
-            # Serialize item into temporary dict
-            temp = item.serialize()
-
-            # Get value to use for key in outer dict
-            key = temp["slug"]
-
-            # Delete key 'name' from dict if exists
-            if temp["name"]:
-                del temp["name"]
-
-            # Populate outer dict value with previously determined key
-            roles[key] = temp
-
-        # passing the data to the parent class so the data is
-        # transformed into actual Host/Group objects
-        super().__init__(devices, roles, **kwargs)
+        # Pass the data back to the parent class
+        super().__init__(devices, None, **kwargs)
