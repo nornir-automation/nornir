d696b16d4fb3390929882fb43219a4ca260107e5
David Barroso <dbarrosop@dravetech.com>
2018-07-10 16:48:12 +0200

    Improvements to filter object

--- tests/core/test_filter.py
+++ tests/core/test_filter.py
@@ -14,41 +14,53 @@ class Test(object):
 
     def test_simple(self):
         f = F(site="site1")
-        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        filtered = sorted(list((inventory.filter(f).hosts.keys())))
 
         assert filtered == ["dev1.group_1", "dev2.group_1"]
 
     def test_and(self):
         f = F(site="site1") & F(role="www")
-        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        filtered = sorted(list((inventory.filter(f).hosts.keys())))
 
         assert filtered == ["dev1.group_1"]
 
     def test_or(self):
         f = F(site="site1") | F(role="www")
-        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        filtered = sorted(list((inventory.filter(f).hosts.keys())))
 
         assert filtered == ["dev1.group_1", "dev2.group_1", "dev3.group_2"]
 
     def test_combined(self):
         f = F(site="site2") | (F(role="www") & F(my_var="comes_from_dev1.group_1"))
-        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        filtered = sorted(list((inventory.filter(f).hosts.keys())))
 
         assert filtered == ["dev1.group_1", "dev3.group_2", "dev4.group_2"]
 
         f = (F(site="site2") | F(role="www")) & F(my_var="comes_from_dev1.group_1")
-        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        filtered = sorted(list((inventory.filter(f).hosts.keys())))
 
         assert filtered == ["dev1.group_1"]
 
     def test_contains(self):
         f = F(groups__contains="group_1")
-        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        filtered = sorted(list((inventory.filter(f).hosts.keys())))
 
         assert filtered == ["dev1.group_1", "dev2.group_1"]
 
     def test_negate(self):
         f = ~F(groups__contains="group_1")
-        filtered = sorted(list((inventory.filter(filter_func=f).hosts.keys())))
+        filtered = sorted(list((inventory.filter(f).hosts.keys())))
 
         assert filtered == ["dev3.group_2", "dev4.group_2"]
+
+    def test_negate_and_second_negate(self):
+        f = F(site="site1") & ~F(role="www")
+        filtered = sorted(list((inventory.filter(f).hosts.keys())))
+
+        assert filtered == ["dev2.group_1"]
+
+    def test_negate_or_both_negate(self):
+        f = ~F(site="site1") | ~F(role="www")
+        filtered = sorted(list((inventory.filter(f).hosts.keys())))
+
+        assert filtered == ["dev2.group_1", "dev3.group_2", "dev4.group_2"]
