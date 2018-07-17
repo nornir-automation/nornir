67440afbd13d4ffdd1997da43f95ada210ca288a
Dmitry Figol <git@dmfigol.me>
2018-07-18 01:16:28 +0300

    Add annotations to nornir.plugins.tasks.data

--- nornir/plugins/tasks/data/load_json.py
+++ nornir/plugins/tasks/data/load_json.py
@@ -1,16 +1,17 @@
 import collections
 import json
+from typing import Dict, MutableMapping, Any, Type
 
-from nornir.core.task import Result
+from nornir.core.task import Result, Task
 
 
-def load_json(task, file, ordered_dict=False):
+def load_json(task: Task, file: str, ordered_dict: bool = False) -> Result:
     """
     Loads a json file.
 
     Arguments:
-        file (str): path to the file containing the json file to load
-        ordered_dict (bool): If set to true used OrderedDict to load maps
+        file: path to the file containing the json file to load
+        ordered_dict: If set to true used OrderedDict to load maps
 
     Examples:
 
@@ -19,12 +20,13 @@ def load_json(task, file, ordered_dict=False):
             > nr.run(task=load_json,
                      file="mydata.json",
                      ordered_dict=True)
+        file: path to the file containing the json file to load
 
     Returns:
-        :obj:`nornir.core.task.Result`:
+        Result object with the following attributes set:
           * result (``dict``): dictionary with the contents of the file
     """
-    kwargs = {}
+    kwargs: Dict[str, Type[MutableMapping[str, Any]]] = {}
     if ordered_dict:
         kwargs["object_pairs_hook"] = collections.OrderedDict
     with open(file, "r") as f:
