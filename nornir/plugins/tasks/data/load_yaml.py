2ac6ef22134dfbb9632866ad192b1889ef79d126
David Barroso <dbarrosop@dravetech.com>
2018-07-17 11:23:55 +0200

    Migrate to ruamel.yaml and add `ordered_dict` parameter to `load_json` and `load_yaml` plugins (#157)

--- nornir/plugins/tasks/data/load_yaml.py
+++ nornir/plugins/tasks/data/load_yaml.py
@@ -1,21 +1,32 @@
 from nornir.core.task import Result
 
+import ruamel.yaml
 
-import yaml
 
-
-def load_yaml(task, file):
+def load_yaml(task, file, ordered_dict=False):
     """
     Loads a yaml file.
 
     Arguments:
         file (str): path to the file containing the yaml file to load
+        ordered_dict (bool): If set to true used OrderedDict to load maps
+
+    Examples:
+
+        Simple example with ``ordered_dict``::
+
+            > nr.run(task=load_yaml,
+                     file="mydata.yaml",
+                     ordered_dict=True)
 
     Returns:
         :obj:`nornir.core.task.Result`:
           * result (``dict``): dictionary with the contents of the file
     """
+    kwargs = {}
+    kwargs["typ"] = "rt" if ordered_dict else "safe"
     with open(file, "r") as f:
-        data = yaml.load(f.read())
+        yml = ruamel.yaml.YAML(pure=True, **kwargs)
+        data = yml.load(f.read())
 
     return Result(host=task.host, result=data)
