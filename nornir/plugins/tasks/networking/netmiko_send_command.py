1cdd805cdba71c7523dce9e45b6a89f1947c3797
Kirk Byers <ktbyers@twb-tech.com>
2018-10-31 10:45:19 -0700

    Add enable argument to Netmiko send_command to make enable support easier

--- nornir/plugins/tasks/networking/netmiko_send_command.py
+++ nornir/plugins/tasks/networking/netmiko_send_command.py
@@ -4,7 +4,11 @@ from nornir.core.task import Result, Task
 
 
 def netmiko_send_command(
-    task: Task, command_string: str, use_timing: bool = False, **kwargs: Any
+    task: Task,
+    command_string: str,
+    use_timing: bool = False,
+    enable: bool = False,
+    **kwargs: Any
 ) -> Result:
     """
     Execute Netmiko send_command method (or send_command_timing)
@@ -19,6 +23,8 @@ def netmiko_send_command(
           * result (``dict``): dictionary with the result of the show command.
     """
     net_connect = task.host.get_connection("netmiko", task.nornir.config)
+    if enable:
+        net_connect.enable()
     if use_timing:
         result = net_connect.send_command_timing(command_string, **kwargs)
     else:
