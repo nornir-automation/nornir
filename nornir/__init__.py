f34cec6f21799520131ab696d0fc89c3936b8fcc
David Barroso <dbarrosop@dravetech.com>
2019-01-12 14:43:48 +0100

    fix nornir's version in documentation (#305)
    * fix nornir's version in documentation
    
    * make pylama happy

--- nornir/__init__.py
+++ nornir/__init__.py
@@ -1,11 +1,4 @@
 from nornir.init_nornir import InitNornir
-import pkg_resources
 
 
-try:
-    __version__ = pkg_resources.get_distribution("nornir").version
-except pkg_resources.DistributionNotFound:
-    __version__ = "Not installed"
-
-
-__all__ = ("InitNornir", "__version__")
+__all__ = ("InitNornir",)
