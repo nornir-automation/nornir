88d9d4517da0b1dea46d25cb15a3d6f1467d3f7e
Dmitry Figol <git@dmfigol.me>
2019-02-27 04:18:57 +0100

    Extract __version__ using pkg_resources

--- nornir/__init__.py
+++ nornir/__init__.py
@@ -1,4 +1,7 @@
+import pkg_resources
+
 from nornir.init_nornir import InitNornir
 
+__version__ = pkg_resources.get_distribution("nornir").version
 
-__all__ = ("InitNornir",)
+__all__ = ("InitNornir", "__version__")
