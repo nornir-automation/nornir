d5eefae47262e34730022bd88ee0d3b5b387b3af
David Barroso <dbarrosop@dravetech.com>
2019-04-27 10:44:46 +0200

    added support to initialize SimpleInventory with native objects (#348)
    * added support to initialize SimpleInventory with native objects
    
    * fix mypy
    
    * fix pylama

--- nornir/plugins/inventory/simple.py
+++ nornir/plugins/inventory/simple.py
@@ -1,8 +1,13 @@
 import logging
 import os
-from typing import Any
+from typing import Any, Optional
 
-from nornir.core.deserializer.inventory import GroupsDict, Inventory, VarsDict
+from nornir.core.deserializer.inventory import (
+    HostsDict,
+    GroupsDict,
+    Inventory,
+    VarsDict,
+)
 
 import ruamel.yaml
 
@@ -15,28 +20,34 @@ class SimpleInventory(Inventory):
         host_file: str = "hosts.yaml",
         group_file: str = "groups.yaml",
         defaults_file: str = "defaults.yaml",
+        hosts: Optional[HostsDict] = None,
+        groups: Optional[GroupsDict] = None,
+        defaults: Optional[VarsDict] = None,
         *args: Any,
         **kwargs: Any
     ) -> None:
-        yml = ruamel.yaml.YAML(typ="safe")
-        with open(host_file, "r") as f:
-            hosts = yml.load(f)
+        if hosts is None:
+            yml = ruamel.yaml.YAML(typ="safe")
+            with open(host_file, "r") as f:
+                hosts = yml.load(f)
 
-        groups: GroupsDict = {}
-        if group_file:
-            if os.path.exists(group_file):
-                with open(group_file, "r") as f:
-                    groups = yml.load(f) or {}
-            else:
-                logger.debug("File %r was not found", group_file)
-                groups = {}
+        if groups is None:
+            groups = {}
+            if group_file:
+                if os.path.exists(group_file):
+                    with open(group_file, "r") as f:
+                        groups = yml.load(f) or {}
+                else:
+                    logger.debug("File %r was not found", group_file)
+                    groups = {}
 
-        defaults: VarsDict = {}
-        if defaults_file:
-            if os.path.exists(defaults_file):
-                with open(defaults_file, "r") as f:
-                    defaults = yml.load(f) or {}
-            else:
-                logger.debug("File %r was not found", defaults_file)
-                defaults = {}
+        if defaults is None:
+            defaults = {}
+            if defaults_file:
+                if os.path.exists(defaults_file):
+                    with open(defaults_file, "r") as f:
+                        defaults = yml.load(f) or {}
+                else:
+                    logger.debug("File %r was not found", defaults_file)
+                    defaults = {}
         super().__init__(hosts=hosts, groups=groups, defaults=defaults, *args, **kwargs)
