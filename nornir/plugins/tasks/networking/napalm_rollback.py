a656be3681b1d8324bbedf97e398477a648fb7ee
Jay Christopher <jay.christopher@autodesk.com>
2019-05-08 14:26:02 -0700

    update param to dry)run and default to None

--- nornir/plugins/tasks/networking/napalm_rollback.py
+++ nornir/plugins/tasks/networking/napalm_rollback.py
@@ -1,16 +1,16 @@
 from nornir.core.task import Result, Task
 
 
-def napalm_rollback(task: Task, rollback: bool = False) -> Result:
+def napalm_rollback(task: Task, dry_run: bool = None) -> Result:
     """
     Rollback device configuration using napalm
 
     Arguments:
-        rollback: whether to rollback the configuration or not
+        dry_run: whether to rollback the configuration or not
     """
     device = task.host.get_connection("napalm", task.nornir.config)
 
-    if rollback:
+    if dry_run:
         device.rollback()
 
     return Result(host=task.host)
