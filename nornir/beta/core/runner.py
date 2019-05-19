7e67e122d273eca6d4dcb20eb3fe4403e90ec440
David Barroso <dbarrosop@dravetech.com>
2019-05-19 20:37:08 +0200

    progress

--- nornir/beta/core/runner.py
+++ nornir/beta/core/runner.py
@@ -1,10 +1,78 @@
 import asyncio
 import logging
+from concurrent.futures import ThreadPoolExecutor
+from dataclasses import asdict, dataclass, field
 from multiprocessing.dummy import Pool
-from typing import Any, Awaitable, Callable, List
+from typing import Any, Awaitable, Callable, Dict, List, Optional, Set
 
 from nornir.core import Nornir
 from nornir.core.inventory import Host
+
+
+@dataclass
+class NornirTaskData:
+    host: Host
+    nornir: Nornir
+    name: str
+
+
+@dataclass
+class Result:
+    ntd: NornirTaskData
+    data: Any = None
+    sub_results: List["Result"] = field(default_factory=list)
+    changed: bool = False
+    diff: str = ""
+    exception: Optional[BaseException] = None
+    severity_level: int = logging.INFO
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__} "{self.ntd.name}": {asdict(self)}'
+
+    def __str__(self) -> str:
+        if self.exception:
+            return str(self.exception)
+
+        else:
+            return str(self.data)
+
+
+@dataclass
+class AggregatedResults(Dict[str, Result]):
+    name: str = ""
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__} ({self.name}): {super().__repr__()}"
+
+
+def _result_wrapper(result: Any, ntd: NornirTaskData) -> Result:
+    if isinstance(result, Result):
+        result.ntd = ntd
+        return result
+    else:
+        return Result(data=result, ntd=ntd)
+
+
+def _func_wrapper(
+    func: Callable[..., Result], ntd: NornirTaskData, *args: Any, **kwargs: Any
+) -> Any:
+    try:
+        return _result_wrapper(func(ntd=ntd, *args, **kwargs), ntd)
+    except Exception as e:
+        return Result(ntd=ntd, exception=e)
+
+
+async def _async_func_wrapper(
+    func: Callable[..., Awaitable[Result]],
+    ntd: NornirTaskData,
+    *args: Any,
+    **kwargs: Any,
+) -> Any:
+    try:
+        result = await func(ntd, *args, **kwargs)
+        return _result_wrapper(result, ntd)
+    except Exception as e:
+        return Result(ntd=ntd, exception=e)
 
 
 class TaskRunner:
@@ -33,39 +101,90 @@ class TaskRunner:
                 if name in nornir.data.failed_hosts:
                     self.hosts.append(host)
 
-    def run(self, **kwargs: Any) -> None:
+    def run(self, **kwargs: Any) -> AggregatedResults:
         if asyncio.iscoroutinefunction(self.task):
-            self._run_async(**kwargs)
+            result = self._run_async(**kwargs)
         else:
-            self._run_sync(**kwargs)
+            result = self._run_sync(**kwargs)
+        agg_results = AggregatedResults(name=self.name)
+        for r in result:
+            agg_results[r.ntd.host.name] = r
+        return agg_results
 
-    def _run_async(self, **kwargs: Any) -> None:
+    def _run_async(self, **kwargs: Any) -> List[Result]:
         loop = asyncio.get_event_loop()
-        hosts = [self.task(host, **kwargs) for host in self.hosts]
-        loop.run_until_complete(asyncio.gather(*hosts))
-        loop.close()
+        hosts = [
+            _async_func_wrapper(
+                self.task,
+                NornirTaskData(host=h, nornir=self.nornir, name=self.name),
+                **kwargs,
+            )
+            for h in self.hosts
+        ]
+        return loop.run_until_complete(asyncio.gather(*hosts))
 
-    def _run_sync(self, **kwargs: Any) -> None:
-        result = {}
-
+    def _run_sync(self, **kwargs: Any) -> List[Result]:
         pool = Pool(processes=self.num_workers)
         result_pool = [
-            pool.apply_async(self.task, args=(h,), kwds=kwargs) for h in self.hosts
+            pool.apply_async(
+                _func_wrapper,
+                args=(
+                    self.task,
+                    NornirTaskData(host=h, nornir=self.nornir, name=self.name),
+                ),
+                kwds=kwargs,
+            )
+            for h in self.hosts
         ]
         pool.close()
         pool.join()
+        return [r.get() for r in result_pool]
 
-        for rp in result_pool:
-            r = rp.get()
-            result[r.host.name] = r
+    def async_with_futures(self, **kwargs: Any) -> Set[Awaitable[Result]]:
+        # TODO: fix return type
+        return {
+            asyncio.ensure_future(
+                _futures_wrapper(
+                    task=self.task,
+                    ntd=NornirTaskData(host=h, nornir=self.nornir, name=self.name),
+                    **kwargs,
+                )
+            )
+            for h in self.hosts
+        }
 
-    def prepare_futures(self, **kwargs: Any) -> List[Awaitable[Any]]:
-        # TODO: fix return types
-        fs: List[Awaitable[Any]] = []
-        for host in self.hosts:
-            f = asyncio.ensure_future(self.task(host, **kwargs))
-            fs.append(f)
+    def sync_with_futures(self, **kwargs: Any) -> Set[Any]:
+        # TODO: fix return type
+        executor = ThreadPoolExecutor(max_workers=self.num_workers)
+        fs = {
+            executor.submit(
+                _func_wrapper,
+                self.task,
+                NornirTaskData(host=h, nornir=self.nornir, name=self.name),
+                **kwargs,
+            )
+            for h in self.hosts
+        }
+        executor.shutdown(wait=False)
         return fs
+
+
+class FutureException(Exception):
+    def __init__(self, exc: Exception, ntd: NornirTaskData) -> None:
+        self.exc = exc
+        self.ntd = ntd
+
+    def __str__(self) -> str:
+        return f"{self.ntd}: {self.exc}"
+
+
+async def _futures_wrapper(
+    task: Callable[..., Any], ntd: NornirTaskData, **kwargs: Any
+) -> Any:
+    try:
+        return await task(ntd, **kwargs)
+    except Exception as e:
+        raise FutureException(ntd=ntd, exc=e)
 
 
 class with_retries:
@@ -80,7 +199,7 @@ class with_retries:
 
     def async_wrapper(self, func: Callable[..., Any]) -> Any:
         async def wrapper(*args: Any, **kwargs: Any) -> Any:
-            for i in range(0, self.attempts):
+            for i in range(1, self.attempts + 1):
                 try:
                     return await func(*args, **kwargs, attempt=i)
                 except Exception:
@@ -91,7 +210,7 @@ class with_retries:
 
     def sync_wrapper(self, func: Callable[..., Any]) -> Any:
         def wrapper(*args: Any, **kwargs: Any) -> Any:
-            for i in range(0, self.attempts):
+            for i in range(1, self.attempts + 1):
                 try:
                     return func(*args, **kwargs, attempt=i)
                 except Exception:
---
+++ tests/beta/core/test_runner.py
@@ -0,0 +1,144 @@
+import asyncio
+import concurrent
+import time
+from typing import Any, Awaitable, List, Set
+
+from nornir.beta.core.runner import (
+    FutureException,
+    NornirTaskData,
+    Result,
+    TaskRunner,
+    with_retries,
+)
+from nornir.core import Nornir
+
+
+@with_retries(attempts=3)
+async def i_fail_async(ntd: NornirTaskData, delay: int, attempt: int) -> Result:
+    await asyncio.sleep(delay)
+    if ntd.host.name == "dev3.group_2":
+        raise Exception("I failed")
+    if ntd.host.name == "dev4.group_2" and attempt < 2:
+        raise Exception("I failed")
+    msg = f"I succeeded at attempt {attempt}"
+    return Result(data=msg, ntd=ntd)
+
+
+@with_retries(attempts=3)
+def i_fail_sync(ntd: NornirTaskData, delay: int, attempt: int) -> Result:
+    time.sleep(delay)
+    if ntd.host.name == "dev3.group_2":
+        raise Exception("I failed")
+    if ntd.host.name == "dev4.group_2" and attempt < 2:
+        raise Exception("I failed")
+    msg = f"I succeeded at attempt {attempt}"
+    return Result(data=msg, ntd=ntd)
+
+
+async def task_async(ntd: NornirTaskData, delay: float) -> Result:
+    await asyncio.sleep(delay)
+    if ntd.host.name == "dev3.group_2":
+        raise Exception("I failed")
+    return Result(data="I was here", ntd=ntd)
+
+
+def task_sync(ntd: NornirTaskData, delay: float) -> Result:
+    time.sleep(delay)
+    if ntd.host.name == "dev3.group_2":
+        raise Exception("I failed")
+    return Result(data="I was here", ntd=ntd)
+
+
+def grouped_task_sync(ntd: NornirTaskData) -> Result:
+    sub_results: List[Result] = []
+    # Make this a bit more seamless
+    r = task_sync(ntd=ntd, delay=0.1)
+    sub_results.append(r)
+    r = task_sync(ntd=ntd, delay=0.1)
+    sub_results.append(r)
+    return Result(ntd=ntd, sub_results=sub_results, data="awesome")
+
+
+class Tests:
+    def test_simple_sync(self, nornir: Nornir) -> None:
+        r = TaskRunner(task=task_sync, nornir=nornir).run(delay=0.1)
+        for host, result in r.items():
+            if host == "dev3.group_2":
+                assert result.exception
+            else:
+                assert not result.exception
+                assert result.data == "I was here"
+
+    def test_simple_async(self, nornir: Nornir) -> None:
+        r = TaskRunner(task=task_async, nornir=nornir).run(delay=0.1)
+        for host, result in r.items():
+            if host == "dev3.group_2":
+                assert result.exception
+            else:
+                assert not result.exception
+                assert result.data == "I was here"
+
+    def test_simple_sync_with_retries(self, nornir: Nornir) -> None:
+        r = TaskRunner(task=i_fail_sync, nornir=nornir).run(delay=0.1)
+        for host, result in r.items():
+            if host == "dev3.group_2":
+                assert result.exception
+            elif host == "dev4.group_2":
+                assert not result.exception
+                assert result.data == "I succeeded at attempt 2"
+            else:
+                assert not result.exception
+                assert result.data == "I succeeded at attempt 1"
+
+    def test_simple_async_with_retries(self, nornir: Nornir) -> None:
+        r = TaskRunner(task=i_fail_async, nornir=nornir).run(delay=0.1)
+        for host, result in r.items():
+            if host == "dev3.group_2":
+                assert result.exception
+            elif host == "dev4.group_2":
+                assert not result.exception
+                assert result.data == "I succeeded at attempt 2"
+            else:
+                assert not result.exception
+                assert result.data == "I succeeded at attempt 1"
+
+    def test_grouped_task_sync(self, nornir: Nornir) -> None:
+        r = TaskRunner(task=grouped_task_sync, nornir=nornir).run()
+        for host, result in r.items():
+            if host == "dev3.group_2":
+                assert result.exception
+            else:
+                assert len(result.sub_results) == 2
+                assert result.data == "awesome"
+
+    def test_async_with_futures(self, nornir: Nornir) -> None:
+        async def done(fs: Set[Awaitable[Any]]) -> None:
+            failed = False
+            for f in asyncio.as_completed(fs):
+                try:
+                    result = await f
+                except FutureException as e:
+                    failed = True
+                    assert e.exc.args[0] == "I failed"
+                    assert e.ntd.host.name == "dev3.group_2"
+                assert not result.exception
+                assert result.data == "I was here"
+            assert failed
+
+        loop = asyncio.get_event_loop()
+        fs = TaskRunner(task=task_async, nornir=nornir).async_with_futures(delay=0.1)
+        loop.run_until_complete(done(fs))
+
+    def test_sync_with_futures(self, nornir: Nornir) -> None:
+        start_time = time.time()
+        fs1 = TaskRunner(task=task_sync, nornir=nornir).sync_with_futures(delay=0.1)
+        fs2 = TaskRunner(task=task_sync, nornir=nornir).sync_with_futures(delay=0.1)
+        for f in concurrent.futures.as_completed({*fs1, *fs2}):
+            result = f.result()
+            if result.ntd.host.name == "dev3.group_2":
+                assert result.exception
+            else:
+                assert not result.exception
+                assert result.data == "I was here"
+        end_time = time.time()
+        assert end_time - start_time < 0.11, end_time - start_time
