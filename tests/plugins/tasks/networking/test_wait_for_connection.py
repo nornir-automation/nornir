ea9b517474b569b98c4f0e000b3db1c3698c5032
szulczew <patryk.szulczewski@nokia.com>
2020-02-15 12:52:11 +0100

    Updated tests with inventory filter to use host and ports from test_tcp_ping

--- tests/plugins/tasks/networking/test_wait_for_connection.py
+++ tests/plugins/tasks/networking/test_wait_for_connection.py
@@ -4,28 +4,22 @@ from tests import skip
 
 
 class Test(object):
-    def test_result(self, nornir):
-        result = nornir.run(
+    @skip
+    def test_dead_hosts(self, nornir):
+        filter = nornir.filter(name="dev4.group_2")
+        result = filter.run(
+            wait_for_connection, port=35004, timeout=1, interval=0, delay=0
+        )
+        assert isinstance(result, AggregatedResult)
+        for h, r in result.items():
+            assert r.failed is True
+
+    @skip
+    def test_online_hosts(self, nornir):
+        filter = nornir.filter(name="dev4.group_2")
+        result = filter.run(
             wait_for_connection, port=22, timeout=1, interval=0, delay=0
         )
         assert isinstance(result, AggregatedResult)
-
-    @skip  # This test can be run against inventory hosts which will not respond on TCP port 2222.
-    def test_dead_hosts(self, nornir):
-        result = nornir.run(
-            wait_for_connection, port=2222, timeout=1, interval=0, delay=0
-        )
-        assert result
-        for h, mr in result.items():
-            for r in mr:
-                assert r.failed is True
-
-    # @skip  # This test can be run against inventory hosts with active TCP port 22
-    # def test_online_hosts(self, nornir):
-    #     result = nornir.run(
-    #         wait_for_connection, port=22, timeout=1, interval=0, delay=0
-    #     )
-    #     assert result
-    #     for h, mr in result.items():
-    #         for r in mr:
-    #             assert r.failed is False
+        for h, r in result.items():
+            assert r.failed is False
