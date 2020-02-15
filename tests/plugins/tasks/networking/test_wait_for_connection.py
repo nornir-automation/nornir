bbe051aecf1d6caf1327e4e481116020ed339f3b
szulczew <patryk.szulczewski@nokia.com>
2020-02-15 09:42:21 +0100

    Fix to MultiResult in test.

--- tests/plugins/tasks/networking/test_wait_for_connection.py
+++ tests/plugins/tasks/networking/test_wait_for_connection.py
@@ -10,14 +10,15 @@ class Test(object):
         )
         assert isinstance(result, AggregatedResult)
 
-    @skip  # This test can be run against inventory hosts which will not respond on TCP port 2222.
+    #@skip  # This test can be run against inventory hosts which will not respond on TCP port 2222.
     def test_dead_hosts(self, nornir):
         result = nornir.run(
             wait_for_connection, port=2222, timeout=1, interval=0, delay=0
         )
         assert result
-        for h, r in result.items():
-            assert r.failed is True
+        for h, mr in result.items():
+            for r in mr:
+                assert r.failed is True
 
     @skip  # This test can be run against inventory hosts with active TCP port 22
     def test_online_hosts(self, nornir):
@@ -25,5 +26,6 @@ class Test(object):
             wait_for_connection, port=22, timeout=1, interval=0, delay=0
         )
         assert result
-        for h, r in result.items():
-            assert r.failed is False
+        for h, mr in result.items():
+            for r in mr:
+                assert r.failed is False
