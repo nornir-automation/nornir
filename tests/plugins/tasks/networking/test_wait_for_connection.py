1336eb81355ff7bc880a32c9cb33ca577c0b4e2d
Patryk Szulczewski <58683518+pszulczewski@users.noreply.github.com>
2020-02-15 13:39:11 +0100

    Hashed out test_online_host in test_waiting_for_connection
    Need clarification from maintainers if inventory has got a host with open port TCP 22.

--- tests/plugins/tasks/networking/test_wait_for_connection.py
+++ tests/plugins/tasks/networking/test_wait_for_connection.py
@@ -14,12 +14,12 @@ class Test(object):
         for h, r in result.items():
             assert r.failed is True
 
-    @skip
-    def test_online_hosts(self, nornir):
-        filter = nornir.filter(name="dev4.group_2")
-        result = filter.run(
-            wait_for_connection, port=22, timeout=1, interval=0, delay=0
-        )
-        assert isinstance(result, AggregatedResult)
-        for h, r in result.items():
-            assert r.failed is False
+    # @skip
+    # def test_online_hosts(self, nornir):
+    #     filter = nornir.filter(name="dev4.group_2")
+    #     result = filter.run(
+    #         wait_for_connection, port=22, timeout=1, interval=0, delay=0
+    #     )
+    #     assert isinstance(result, AggregatedResult)
+    #     for h, r in result.items():
+    #         assert r.failed is False
