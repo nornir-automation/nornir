9f1891bb8fe3d8c78aa597b43f8c4c6e484315c8
szulczew <patryk.szulczewski@nokia.com>
2020-02-15 08:53:34 +0100

    Added network plugin 'wait_for_connection'.

---
+++ nornir/plugins/tasks/networking/wait_for_connection.py
@@ -0,0 +1,52 @@
+import socket
+import time
+from datetime import datetime, timedelta
+from typing import Optional
+
+from nornir.core.task import Result, Task
+
+
+class TimedOut(Exception):
+    pass
+
+
+def wait_for_connection(
+    task: Task,
+    port: int,
+    delay: int = 0,
+    timeout: int = 600,
+    interval: int = 1,
+    host: Optional[str] = None,
+) -> Result:
+    """
+    Wait for connection on a tcp port.
+    Args:
+        task: Task object
+        port: TCP port
+        delay: time before start polling a device
+        timeout: timeout after a node will be marked as fail
+        interval: time between polls
+        host: hostname or ip address
+
+    Returns:
+        Result object
+
+    """
+    host = host or task.host
+    end_time = datetime.now() + timedelta(seconds=timeout) + timedelta(seconds=delay)
+    time.sleep(delay)
+    while datetime.now() < end_time:
+        s = socket.socket()
+        s.settimeout(5)
+        try:
+            status = s.connect_ex((task.host.hostname, port))
+            if status == 0:
+                print(f"{host} {task.host.hostname} is online")
+                return Result(host=task.host, result=None)
+            else:
+                time.sleep(interval)
+        except (socket.gaierror, socket.timeout, socket.error) as e:
+            print(e)
+        finally:
+            s.close()
+    raise TimedOut("Timed out waiting for: {}".format(host))
