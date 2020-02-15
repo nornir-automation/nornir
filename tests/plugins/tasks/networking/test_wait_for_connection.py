6e306f9f692ca57f8d4318962cf2f39887d2df61
szulczew <patryk.szulczewski@nokia.com>
2020-02-15 10:05:17 +0100

    Looks like there are no hosts in test inventory which can give response on TCP 22. Hashing out test_online from tests.

--- tests/plugins/tasks/networking/test_wait_for_connection.py
+++ tests/plugins/tasks/networking/test_wait_for_connection.py
@@ -20,12 +20,12 @@ class Test(object):
             for r in mr:
                 assert r.failed is True
 
-    @skip  # This test can be run against inventory hosts with active TCP port 22
-    def test_online_hosts(self, nornir):
-        result = nornir.run(
-            wait_for_connection, port=22, timeout=1, interval=0, delay=0
-        )
-        assert result
-        for h, mr in result.items():
-            for r in mr:
-                assert r.failed is False
+    # @skip  # This test can be run against inventory hosts with active TCP port 22
+    # def test_online_hosts(self, nornir):
+    #     result = nornir.run(
+    #         wait_for_connection, port=22, timeout=1, interval=0, delay=0
+    #     )
+    #     assert result
+    #     for h, mr in result.items():
+    #         for r in mr:
+    #             assert r.failed is False
