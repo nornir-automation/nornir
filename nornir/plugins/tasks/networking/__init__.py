ab718ecab779f1e80611bd5c15cce41f4c03d2c6
Tejas Mokashi <58966340+tejasmokashi1992@users.noreply.github.com>
2020-03-29 16:22:13 +0530

    Update __init__.py

--- nornir/plugins/tasks/networking/__init__.py
+++ nornir/plugins/tasks/networking/__init__.py
@@ -12,7 +12,7 @@ from .netmiko_send_command import netmiko_send_command
 from .netmiko_send_config import netmiko_send_config
 from .netmiko_save_config import netmiko_save_config
 from .tcp_ping import tcp_ping
-from .napalm_ping import napalm_ping
+from .napalm_traceroute import napalm_traceroute
 
 __all__ = (
     "napalm_cli",
@@ -30,4 +30,5 @@ __all__ = (
     "netmiko_save_config",
     "tcp_ping",
     "napalm_ping",
+    "napalm_traceroute",
 )
