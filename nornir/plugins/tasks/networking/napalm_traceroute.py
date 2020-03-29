f1efceb2e9921de2cdc58b2d78edf0364e17b305
Tejas Mokashi <58966340+tejasmokashi1992@users.noreply.github.com>
2020-03-29 15:41:40 +0530

    napalm_traceroute.py

--- nornir/plugins/tasks/networking/napalm_traceroute.py
+++ nornir/plugins/tasks/networking/napalm_traceroute.py
@@ -12,7 +12,6 @@ def napalm_traceroute(
 ) -> Result:
 
     """
-    
     Executes traceroute on the device and returns a dictionary with the result.
     Arguments:
       dest(str) – Host or IP Address of the destination.
@@ -20,7 +19,6 @@ def napalm_traceroute(
       ttl(int, optional) – Max number of hops.
       timeout(int, optional) – Max seconds to wait after sending final packet.
       vrf(str, optional) - Name of vrf.
-      
     Examples:
       Simple example::
             > nr.run(task=napalm_traceroute,
@@ -28,21 +26,14 @@ def napalm_traceroute(
       Passing some other optional arguments::
             > nr.run(task=napalm_traceroute,
             >        dest='8.8.8.8', source='10.1.1.2', ttl=100, timeout=1)
-            
     Returns:
        Result object with the following attributes set:
        * result (``dict``): list of dictionary with the result of the trace response.
          Output dictionary has one of following keys "success or error"
-         
     """
-    
+
     device = task.host.get_connection("napalm", task.nornir.config)
     result = device.traceroute(
-        destination=dest,
-        source=source,
-        ttl=ttl,
-        timeout=timeout,
-        vrf=vrf,
+        destination=dest, source=source, ttl=ttl, timeout=timeout, vrf=vrf,
     )
     return Result(host=task.host, result=result)
-
