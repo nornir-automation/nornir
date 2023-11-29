395d0025e7f1d4747b018bfd086d7e096c67e660
Damien Garros <dgarros@gmail.com>
2023-11-29 12:27:29 +0100

    Leverage Async for InitNornir and inventory_load

--- nornir/init_nornir.py
+++ nornir/init_nornir.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+import asyncio
+
 from nornir.core import Nornir
 from nornir.core.configuration import Config
 from nornir.core.inventory import Inventory
@@ -15,17 +17,36 @@ from nornir.core.state import GlobalState
 def load_inventory(
     config: Config,
 ) -> Inventory:
+    return asyncio.run(load_inventory_async(config=config))
+
+
+async def load_inventory_async(
+    config: Config,
+) -> Inventory:
     InventoryPluginRegister.auto_register()
     inventory_plugin = InventoryPluginRegister.get_plugin(config.inventory.plugin)
-    inv = inventory_plugin(**config.inventory.options).load()
+    inventory = inventory_plugin(**config.inventory.options)
+    if asyncio.iscoroutinefunction(inventory.load):
+        inv = await inventory.load()
+    else:
+        inv = inventory.load()
 
     if config.inventory.transform_function:
         TransformFunctionRegister.auto_register()
         transform_function = TransformFunctionRegister.get_plugin(
             config.inventory.transform_function
         )
-        for h in inv.hosts.values():
-            transform_function(h, **(config.inventory.transform_function_options or {}))
+
+        if asyncio.iscoroutinefunction(transform_function):
+            for h in inv.hosts.values():
+                await transform_function(
+                    h, **(config.inventory.transform_function_options or {})
+                )
+        else:
+            for h in inv.hosts.values():
+                transform_function(
+                    h, **(config.inventory.transform_function_options or {})
+                )
 
     return inv
 
@@ -39,7 +60,7 @@ def load_runner(
     return runner
 
 
-def InitNornir(
+async def InitNornirAsync(
     config_file: str = "",
     dry_run: bool = False,
     **kwargs: Any,
@@ -69,8 +90,31 @@ def InitNornir(
     config.logging.configure()
 
     return Nornir(
-        inventory=load_inventory(config),
+        inventory=await load_inventory_async(config),
         runner=load_runner(config),
         config=config,
         data=data,
+    )
+
+
+def InitNornir(
+    config_file: str = "",
+    dry_run: bool = False,
+    **kwargs: Any,
+) -> Nornir:
+    """
+    Arguments:
+        config_file(str): Path to the configuration file (optional)
+        dry_run(bool): Whether to simulate changes or not
+        configure_logging: Whether to configure logging or not. This argument is being
+            deprecated. Please use logging.enabled parameter in the configuration
+            instead.
+        **kwargs: Extra information to pass to the
+            :obj:`nornir.core.configuration.Config` object
+
+    Returns:
+        :obj:`nornir.core.Nornir`: fully instantiated and configured
+    """
+    return asyncio.run(
+        InitNornirAsync(config_file=config_file, dry_run=dry_run, **kwargs)
     )
